afeb24ef2ee2e7b1497731397d3f83360e0964ba
jainmayur517 <jainmayur517@gmail.com>
2021-02-14 02:43:05 +0530

    f

--- index.js
+++ index.js
@@ -49,7 +49,15 @@ Meme.create(
 
 )
 */
+app.use((req,res,next) => {
+res.header("Access-Control-Allow-Origin","*");
+res.header("Access-Control-Allow-Headers","*");
 
+if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+}
+});
 
 
 app.get("/memes",function(req,res){
